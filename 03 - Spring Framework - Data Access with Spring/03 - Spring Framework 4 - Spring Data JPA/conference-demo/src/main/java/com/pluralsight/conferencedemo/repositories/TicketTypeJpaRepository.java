490184b08b341de02fa86b03397f479724d18ca1
José Nildo de Macedo Pereira <jose-nildo@MacBook-Pro-de-Jose.local>
2023-09-25 16:13:43 -0300

    Add: Implementar novos metodos de pesquisa
    Implementado novos metodos customizados de pesquisa

--- 03 - Spring Framework - Data Access with Spring/03 - Spring Framework 4 - Spring Data JPA/conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TicketTypeJpaRepository.java
+++ 03 - Spring Framework - Data Access with Spring/03 - Spring Framework 4 - Spring Data JPA/conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TicketTypeJpaRepository.java
@@ -1,10 +1,32 @@
 package com.pluralsight.conferencedemo.repositories;
 
+import com.pluralsight.conferencedemo.models.TicketPrice;
 import com.pluralsight.conferencedemo.models.TicketType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface TicketTypeJpaRepository extends JpaRepository<TicketType, Long> {
-    List<TicketType> findByIncludesWorkshopTrue();
+
+    @Query("select tp from TicketPrice tp where tp.basePrice > ?1" + "and tp.ticketType.includesWorkshop = true")
+    List<TicketType> findByIncludesWorkshopTrue(BigDecimal maxPrice);
+
+    List<TicketPrice> nameFindTicketsByPricingCategoryName(@Param("name") String name);
+
+    List<TicketPrice> nativeFindTicketsByCategoryWithWorkshop(@Param("name") String name);
 }
+
+
+
+
+
+
+
+
+
+
+
+
